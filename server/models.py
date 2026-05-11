d7307e254886da1971aba728c5693a664df14b21
Joseph Ndemo <joseph.ndemo@student.moringaschool.com>
2026-05-11 02:30:35 -0400

    1st commit

--- server/models.py
+++ server/models.py
@@ -13,7 +13,7 @@ db = SQLAlchemy(metadata=metadata)
 class Pet(db.Model):
     __tablename__ = 'pets'
 
-    id = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
 
